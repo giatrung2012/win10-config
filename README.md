0c9fe5c6bb7ac26d2edd71c33ad7ee610bf6074c
giatrung2012 <trunghsg2012@gmail.com>
2023-04-10 21:03:51 +0700

    Implement

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 ## Post setup
 - Windows Update -> install all update & drivers
-- Import files in task-scheduler/
+- Import files in task-scheduler/ & change user account
 - Add "DOC DOCX PPT PPTX XLS XLSX" to IDM file types
 - Enable wider and disable notification in f.lux
-- Datetime settings: Control Panel -> Change date, time,... -> https://imgur.com/GDif8qS
+- Control Panel -> Change date, time,... -> https://imgur.com/GDif8qS
 - Settings -> System -> Multitasking -> Alt + Tab -> Open windows only
 - Settings -> Ease of Access -> Keyboard -> Turn off Tickey Keys
-- Disable "turn off device to save power" in Device Manager
+- Device Manager -> Disable "turn off device to save power" in Wifi driver
--- setup.ps1
+++ setup.ps1
@@ -6,33 +6,33 @@ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
 
 # Main
-scoop install 7zip aria2 innounp wixtoolset winfetch fzf rclone git pwsh sudo
+scoop install 7zip aria2 winfetch fzf rclone git pwsh sudo
 
 # Extras
 scoop bucket add extras
-scoop install authy notepadplusplus lockhunter
+scoop install notepadplusplus lockhunter authy
 
 # Nerd fonts
 scoop bucket add nerd-fonts
-scoop install JetBrainsMono-NF JetBrains-Mono
+scoop install JetBrains-Mono
 
 # Nonportable
 scoop bucket add nonportable
 scoop install office-365-apps-minimal-np
 
 # Config
-cp .\config\notepadplusplus\config.xml $HOME\scoop\apps\notepadplusplus\current\
-curl -L https://raw.githubusercontent.com/dracula/notepad-plus-plus/master/Dracula.xml --output $HOME\scoop\apps\notepadplusplus\current\themes\Dracula.xml
-cp .\config\.gitconfig $HOME
-
-# Scoop checkup
-sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+cp $HOME\win10-scripts\config\notepadplusplus\config.xml $HOME\scoop\apps\notepadplusplus\current\
+curl -L https://raw.githubusercontent.com/60ss/Npp-1-Dark/master/Npp-1-Dark.xml --output $HOME\scoop\apps\notepadplusplus\current\themes\Npp-1-Dark.xml
+cp $HOME\win10-scripts\config\.gitconfig $HOME
 
 # Context menu
-sudo reg import $HOME\scoop\apps\7zip\current\install-context.reg
-sudo reg import $HOME\scoop\apps\pwsh\current\install-context.reg
-sudo reg import $HOME\scoop\apps\notepadplusplus\current\install-context.reg
-sudo reg import $HOME\scoop\apps\lockhunter\current\install-context.reg
+reg import $HOME\scoop\apps\7zip\current\install-context.reg
+reg import $HOME\scoop\apps\pwsh\current\install-explorer-context.reg
+reg import $HOME\scoop\apps\notepadplusplus\current\install-context.reg
+reg import $HOME\scoop\apps\lockhunter\current\install-context.reg
+
+# Dual boot
+sudo reg import $HOME\win10-scripts\scripts\fix-clock-for-dual-boot.reg
 
 # Active IDM
 iwr -useb https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IAS.ps1 | iex
